ee64b422c3e45e7fc99dc80e669d156b3f904e42
maleeha <74322882+maliaistrying@users.noreply.github.com>
2025-02-24 16:20:43 +0300

    Delete app/(auth)/signup/Login directory

--- app/(auth)/signup/Login/page.js
+++
@@ -1,9 +0,0 @@
-import LoginForm from "@/components/LoginForm";
-
-export default function LoginPage() { // Changed from LoginForm to LoginPage
-  return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <LoginForm />
-    </div>
-  );
-}
