9d9d680b74746fdf4a52712481eb796c9b220646
04maryam <itsmaryamali54@gmail.com>
2025-03-01 16:44:21 +0300

    dashboard

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -3,28 +3,77 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { db, collection, getDocs } from "@/lib/firebase";
-import { auth } from "@/lib/firebase"; // Ensure you import your auth configuration
+import { auth } from "@/lib/firebase";
 import { onAuthStateChanged } from "firebase/auth";
+import {
+  Popover,
+  PopoverButton,
+  PopoverGroup,
+  PopoverPanel,
+} from "@headlessui/react";
+import {
+  ArrowPathIcon,
+  Bars3Icon,
+  ChartPieIcon,
+  CursorArrowRaysIcon,
+  FingerPrintIcon,
+  SquaresPlusIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import styles from "../styles/Dashboard.module.css";
 
+const products = [
+  {
+    name: "Analytics",
+    description: "Get a better understanding of your traffic",
+    href: "#",
+    icon: ChartPieIcon,
+  },
+  {
+    name: "Engagement",
+    description: "Speak directly to your customers",
+    href: "#",
+    icon: CursorArrowRaysIcon,
+  },
+  {
+    name: "Security",
+    description: "Your customers' data will be safe and secure",
+    href: "#",
+    icon: FingerPrintIcon,
+  },
+  {
+    name: "Integrations",
+    description: "Connect with third-party tools",
+    href: "#",
+    icon: SquaresPlusIcon,
+  },
+  {
+    name: "Automations",
+    description: "Build strategic funnels that will convert",
+    href: "#",
+    icon: ArrowPathIcon,
+  },
+];
 export default function Dashboard() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [filter, setFilter] = useState("all");
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // State to track if user is logged in
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
-        setIsLoggedIn(true); // User is logged in
-        fetchProducts(); // Fetch products only when logged in
+        setIsLoggedIn(true);
+        fetchProducts();
       } else {
-        setIsLoggedIn(false); // User is logged out
-        router.push("/login"); // Redirect to login if not logged in
+        setIsLoggedIn(false);
+        router.push("/login");
       }
     });
-
     return () => unsubscribe();
   }, [router]);
 
@@ -36,7 +85,7 @@ export default function Dashboard() {
         ...doc.data(),
       }));
       setProducts(productsList);
-      setFilteredProducts(productsList); // Set initial filtered products
+      setFilteredProducts(productsList);
     } catch (error) {
       console.error("Error fetching products:", error);
     } finally {
@@ -46,118 +95,137 @@ export default function Dashboard() {
 
   useEffect(() => {
     if (filter === "highest") {
-      const sortedProducts = [...products].sort((a, b) => b.quantity - a.quantity);
-      setFilteredProducts(sortedProducts);
+      setFilteredProducts(
+        [...products].sort((a, b) => b.quantity - a.quantity)
+      );
     } else if (filter === "lowest") {
-      const sortedProducts = [...products].sort((a, b) => a.quantity - b.quantity);
-      setFilteredProducts(sortedProducts);
+      setFilteredProducts(
+        [...products].sort((a, b) => a.quantity - b.quantity)
+      );
     } else {
-      setFilteredProducts(products); // Show all products if "all" is selected
+      setFilteredProducts(products);
     }
   }, [filter, products]);
 
-  const styles = {
-    dashboardContainer: {
-      padding: '20px',
-      backgroundColor: '#ffffff',
-    },
-    title: {
-      fontSize: '24px',
-      fontWeight: 'bold',
-      color: '#333333',
-    },
-    subtitle: {
-      fontSize: '18px',
-      color: '#555555',
-    },
+  const style = {
     loadingText: {
-      color: '#0070f3',
+      color: "#0070f3",
     },
     noProductsText: {
-      color: '#ff0000',
+      color: "#ff0000",
     },
     productsGrid: {
-      display: 'grid',
-      gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))',
-      gap: '20px',
+      display: "grid",
+      gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
+      gap: "20px",
     },
     productCard: {
-      border: '1px solid #ddd',
-      padding: '10px',
-      borderRadius: '8px',
-      backgroundColor: '#f9f9f9',
+      border: "1px solid #ddd",
+      padding: "10px",
+      borderRadius: "8px",
+      backgroundColor: "#f9f9f9",
     },
     productImage: {
-      width: '100%',
-      height: 'auto',
+      width: "100%",
+      height: "auto",
     },
     productName: {
-      fontSize: '20px',
-      fontWeight: 'bold',
-      color: '#333333',
+      fontSize: "20px",
+      fontWeight: "bold",
+      color: "#333333",
     },
     productQuantity: {
-      fontSize: '16px',
-      color: '#333333',
+      fontSize: "16px",
+      color: "#333333",
     },
     productDescription: {
-      fontSize: '16px',
-      color: '#333333',
-    },
-    button: {
-      padding: '10px 15px',
-      backgroundColor: '#0070f3',
-      color: '#ffffff',
-      border: 'none',
-      borderRadius: '5px',
-      cursor: 'pointer',
-      marginRight: '10px',
+      fontSize: "16px",
+      color: "#333333",
     },
   };
 
   if (loading) {
-    return <p style={styles.loadingText}>Loading products...</p>; // Loading state
+    return (
+      <div className={styles.dashboardContainer}>
+        <p style={style.noProductsText}>Loading products...</p>
+      </div>
+    );
   }
 
   if (!isLoggedIn) {
-    return null; // Or you can return a loading state or null
+    return null;
   }
 
   return (
-    <div style={styles.dashboardContainer}>
-      <h2 style={styles.title}>Dashboard</h2>
-      <p style={styles.subtitle}>Overview of all added products</p>
-
-      <div>
-        <button style={styles.button} onClick={() => setFilter("highest")}>
-          Products with Highest Quantity
-        </button>
-        <button style={styles.button} onClick={() => setFilter("lowest")}>
-          Products with Lowest Quantity
-        </button>
-        <button style={styles.button} onClick={() => setFilter("all")}>
-          Show All Products
-        </button>
+    <div className={styles.dashboardContainer}>
+      <div className={styles.dashboardHeader}>
+        <div>
+          <h2 className={styles.title}>Dashboard</h2>
+          <p className={styles.subtitle}>Overview of all added products</p>
+        </div>
+        <div>
+          <button
+            className={styles.filterButton}
+            onClick={() => setDropdownOpen(!dropdownOpen)}
+          >
+            Filter Products <i className="bi bi-funnel"></i>
+          </button>
+          {dropdownOpen && (
+            <div className={styles.filtersList}>
+              <ul>
+                <li className={styles.listItem}>
+                  <button
+                    className={styles.filter}
+                    onClick={() => {
+                      setFilter("highest");
+                      setDropdownOpen(false);
+                    }}
+                  >
+                    Highest Quantity
+                  </button>
+                </li>
+                <li className={styles.listItem}>
+                  <button
+                    className={styles.filter}
+                    onClick={() => {
+                      setFilter("lowest");
+                      setDropdownOpen(false);
+                    }}
+                  >
+                    Lowest Quantity
+                  </button>
+                </li>
+                <li className={styles.listItem}>
+                  <button
+                    className={styles.filter}
+                    onClick={() => {
+                      setFilter("all");
+                      setDropdownOpen(false);
+                    }}
+                  >
+                    Clear
+                  </button>
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
 
       {filteredProducts.length === 0 ? (
-        <p style={styles.noProductsText}>No products found.</p>
+        <p style={style.noProductsText}>No products found.</p>
       ) : (
-        <div style={styles.productsGrid}>
+        <div style={style.productsGrid}>
           {filteredProducts.map((product) => (
-            <div key={product.id} style={styles.productCard}>
+            <div key={product.id} style={style.productCard}>
               <img
                 src={product.image}
                 alt={product.name}
-                style={styles.productImage}
+                style={style.productImage}
               />
-              <h3 style={styles.productName}>{product.name}</h3>
-              <p style={styles.productQuantity}>
-                Quantity: {product.quantity}
-              </p>
-              <p style={styles.productDescription}>
-                {product.description}
-              </p>
+              <h3 style={style.productName}>{product.name}</h3>
+              <p style={style.productQuantity}>Quantity: {product.quantity}</p>
+              <p style={style.productDescription}>{product.description}</p>
             </div>
           ))}
         </div>
